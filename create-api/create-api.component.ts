49720011543be513af27d90d274d2e46a4abd493
Muskan <96124700+Miko-29@users.noreply.github.com>
2025-05-15 11:36:29 +0530

    sql draft

---
+++ create-api/create-api.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-api',
+  templateUrl: './create-api.component.html',
+  styleUrls: ['./create-api.component.scss']
+})
+export class CreateApiComponent {
+
+}
